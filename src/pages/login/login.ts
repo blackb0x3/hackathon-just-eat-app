3bc739e383938c5ba467d15792106183ff483935
MarloweWhite <marlowe_white@yahoo.co.uk>
2018-11-24 16:57:18 +0000

    Login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {FormBuilder, FormGroup, Validators, AbstractControl} from '@angular/forms';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 /**
  * Generated class for the LoginPage page.
@@ -15,11 +17,27 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  formgroup:FormGroup;
+
+
+  username:string;
+  password:string;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public formbuilder:FormBuilder) {
+
+
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
   }
 
+  login(){
+  
+    console.log("Username: " + this.username);
+    console.log("Password: " + this.password);
+  }
+
+  // goRegister(){
+  //   this.navCtrl.push();
+  // }
 }
